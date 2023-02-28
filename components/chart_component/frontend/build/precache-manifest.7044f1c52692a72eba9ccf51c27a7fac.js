df5bea9cb49959a07170a1958b506195ace7f0a0
Ferdinand Meyer <ferdinand.meyer.wi@gmail.com>
2023-02-28 18:33:04 +0100

    chart component fix

--- components/chart_component/frontend/build/precache-manifest.7044f1c52692a72eba9ccf51c27a7fac.js
+++ components/chart_component/frontend/build/precache-manifest.7044f1c52692a72eba9ccf51c27a7fac.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7a846b5427d75c647aa8fe59071abeef",
+    "revision": "b562f8b72179b67f6ea0566e3fb3714b",
     "url": "./index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.edf123df.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e097e75026c736aaf99f",
-    "url": "./static/js/main.04fe6122.chunk.js"
+    "revision": "6fb8687a8b5d4b60c749",
+    "url": "./static/js/main.4b17d747.chunk.js"
   },
   {
     "revision": "5657752c0e37b763beb2",
